95f39455ac1352782082fa1de91c0cf9a4893988
ryanjj-49 <seniorderick9@gmail.com>
2026-06-24 23:03:45 +0300

    Upload files - Teddyxcloud

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    name: "DARK-PRINCE-MD",
+    script: "index.js",
+    cwd: "/root/web/gifted-session",
+    env: {
+      PORT: 6898,
+      DATAASE_URL: ""
+    }
+  }]
+};
